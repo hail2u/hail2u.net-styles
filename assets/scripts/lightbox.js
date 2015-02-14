a6d9bd5f9d64896910fa4bd6e47a4c7a65a17ef1
Kyo Nagashima <hail2u@gmail.com>
2015-02-15 00:42:34 +0900

    Test lightbox style image viewer

---
+++ assets/scripts/lightbox.js
@@ -0,0 +1,73 @@
+(function () {
+  if (
+    !('querySelectorAll' in document) ||
+    !('bind' in Function)
+  ) {
+    return;
+  }
+
+  var toggle = function (image, evt) {
+    if (evt.which !== 1) {
+      return;
+    }
+
+    evt.preventDefault();
+
+    if (this.style.cssText && image.style.cssText) {
+      if (image._src) {
+        image.src = image._src;
+        delete image._src;
+      }
+
+      this.style.cssText = '';
+      image.style.cssText = '';
+
+      return;
+    }
+
+    if (this.href !== image.src) {
+      image._src = image.src;
+      image.src = this.href;
+    }
+
+    this.style.backgroundColor = '#fff';
+    this.style.cursor = 'zoom-out';
+    this.style.height = '100vh';
+    this.style.left = '0';
+    this.style.position = 'fixed';
+    this.style.top = '0';
+    this.style.width = '100vw';
+    this.style.zIndex = '2';
+    image.style.bottom = '0';
+    image.style.height = 'auto';
+    image.style.left = '0';
+    image.style.margin = 'auto';
+    image.style.maxHeight = '96%';
+    image.style.maxWidth = '96%';
+    image.style.position = 'absolute';
+    image.style.right = '0';
+    image.style.top = '0';
+    image.style.width = 'auto';
+  };
+
+  var lightbox = function () {
+    var images = document.querySelectorAll('main img[src^="assets/images/"], main img[src^="/images/"]');
+    var image;
+    var parent;
+
+    for (var i = 0, l = images.length; i < l; i++) {
+      image = images[i];
+      parent = image.parentNode;
+
+      if (parent.tagName === 'A' && /^(assets)?\/images\//.test(parent.getAttribute('href'))) {
+        parent.addEventListener('click', toggle.bind(parent, image), false);
+      }
+    }
+  };
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', lightbox, false);
+  } else {
+    lightbox();
+  }
+})();
