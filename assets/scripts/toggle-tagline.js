a2f9ec68ab38d4e3d0f0919005baa2b408d5076a
Kyo Nagashima <hail2u@gmail.com>
2015-02-23 05:24:46 +0900

    Increase "First Heading" display on permalink

--- assets/scripts/toggle-tagline.js
+++ assets/scripts/toggle-tagline.js
@@ -1,12 +1,17 @@
 (function () {
+  'use strict';
+
   var t = function () {
     var logo = document.querySelector('.logo');
     var tagline = document.querySelector('.tagline');
+    var article = document.getElementById('introduction');
     logo.addEventListener('click', function (evt) {
       if (tagline.hidden) {
         tagline.hidden = false;
+        article.removeAttribute('role');
       } else {
         tagline.hidden = true;
+        article.setAttribute('role', 'main');
       }
 
       evt.preventDefault();
