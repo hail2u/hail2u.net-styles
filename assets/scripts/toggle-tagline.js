fff6139463829ef1a084c74f600426a19bb5cffb
Kyo Nagashima <hail2u@gmail.com>
2015-10-12 08:46:19 +0900

    Update assets

--- assets/scripts/toggle-tagline.js
+++ assets/scripts/toggle-tagline.js
@@ -1,24 +1,21 @@
 (function (d) {
   "use strict";
 
-  var toggle = function (tagline, article, evt) {
+  var toggle = function (tagline, html, evt) {
     if (tagline.hidden) {
       tagline.hidden = false;
-      article.removeAttribute("role");
     } else {
       tagline.hidden = true;
-      article.setAttribute("role", "main");
     }
 
+    html.classList.toggle("permalink");
     evt.preventDefault();
   };
 
   var init = function () {
-    var article = d.getElementById("introduction");
-    var tagline = d.querySelector(".tagline");
     d.querySelector(".first-heading").addEventListener(
       "click",
-      toggle.bind(null, tagline, article),
+      toggle.bind(null, d.querySelector(".tagline"), d.documentElement),
       false
     );
   };
